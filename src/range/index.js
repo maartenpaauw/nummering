bf4ab24577089f7ef8d7087e8e292bf5221bf151
Maarten Paauw <maartenpaauw@gmail.com>
2017-05-20 22:01:41 +0200

    Fixed min value for a number #2

--- src/range/index.js
+++ src/range/index.js
@@ -3,6 +3,6 @@ import addZero from 'add-zero'
 
 export default function generate (start, end, step, prefix, leadingZeros, suffix) {
   return _.map(_.range(start, (end + 1), step), (number) => {
-    return `${prefix}${addZero(number, leadingZeros)}${suffix}`.trim()
+    return `${prefix}${addZero(number, leadingZeros + 1)}${suffix}`.trim()
   })
 }
