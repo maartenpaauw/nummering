d13dced2a69327152b1bbedaf72079afa6ac2ab4
Maarten Paauw <maartenpaauw@gmail.com>
2017-05-21 16:05:35 +0200

    Vuex cleanup

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -2,7 +2,7 @@ import * as types from './mutation-types'
 
 export default {
   [types.SETTING_UPDATED] (state, { setting, value }) {
-    state.settings[setting].value = value
+    state.settings[setting] = value
   },
   [types.MENU_TOGGLED] (state, { value }) {
     state.visible.menu = value
--- src/store/state.js
+++ src/store/state.js
@@ -1,30 +1,7 @@
 import { values } from '../settings'
 
 const settings = {
-  filename: {
-    value: values.filename
-  },
-  header: {
-    value: values.header
-  },
-  start: {
-    value: values.start
-  },
-  end: {
-    value: values.end
-  },
-  step: {
-    value: values.step
-  },
-  prefix: {
-    value: values.prefix
-  },
-  suffix: {
-    value: values.suffix
-  },
-  leading_zeros: {
-    value: values.leading_zeros
-  }
+  ...values
 }
 
 const visible = {
