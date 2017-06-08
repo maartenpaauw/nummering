9b14b39a2a84feac200eadbfc22940a2d9782f56
Maarten Paauw <maartenpaauw@gmail.com>
2017-06-08 13:38:12 +0200

    Splitted up the routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import About from '@/components/About'
-import Generator from '@/components/Generator'
-import NotFound from '@/components/NotFound'
+import routes from './routes'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'generator',
-      component: Generator
-    },
-    {
-      path: '/deze-tool',
-      name: 'about',
-      component: About
-    },
-    {
-      path: '*',
-      name: 'not_found',
-      component: NotFound
-    }
-  ]
+  routes
 })
