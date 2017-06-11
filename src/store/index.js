5cd1631ad6ae035183419c5786788bcfe45da3b8
Maarten Paauw <maartenpaauw@gmail.com>
2017-06-11 18:25:22 +0200

    Store order

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,19 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import * as actions from './actions'
-import { getters } from './getters'
-import mutations from './mutations'
+
 import { state } from './state'
+import { getters } from './getters'
+import * as actions from './actions'
+import mutations from './mutations'
+
 import { plugins } from './plugins'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  actions,
-  getters,
-  mutations,
   state,
+  getters,
+  actions,
+  mutations,
   plugins
 })
