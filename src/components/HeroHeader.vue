07007aa9abdbcc9af5d711af743d715a55ce4c17
Maarten Paauw <maartenpaauw@gmail.com>
2017-06-26 10:42:48 +0200

    Logo

--- src/components/HeroHeader.vue
+++ src/components/HeroHeader.vue
@@ -2,21 +2,36 @@
   .hero-head
     header.nav
       .container
+        .nav-left
+          a.nav-item.is-active
+            span.icon
+              i.fa.fa-file-excel-o
+            span.brand(v-html="brand")
         .nav-center
           a.nav-item(href="https://github.com/maartenpaauw/nummering",
                      target="_blank",
                      :title="title")
             span.icon
               i.fa.fa-github
+        .nav-right
 </template>
 
 <script>
   export default {
     name: 'v-hero-header',
     computed: {
+      brand () {
+        return this.$t('hero.title')
+      },
       title () {
         return this.$t('header.on_github')
       }
     }
   }
 </script>
+
+<style lang="scss" scoped="scoped">
+  .brand {
+    margin-left: 0.25rem;
+  }
+</style>
